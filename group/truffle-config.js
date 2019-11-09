de3981249b592a28c38e8a733bfd2e474d619fc7
EG <ethereumdev87@gmail.com>
2019-11-09 03:11:08 -0800

    update

--- group/truffle-config.js
+++ group/truffle-config.js
@@ -1,9 +1,6 @@
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "exact husband cup journey give beauty best fox rapid ripple aisle stairs company coach notice";
 
 // truffle test --network <network-name>
 
@@ -11,38 +8,17 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
       network_id: "5777"       // Any network (default: none)
     },
     rinkeby: {
-      host: "127.0.0.1",
-      port: 8545,
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/21fea1b2e4514a24b4fb2b3fd089b9ee");
+      },
+      host: "localhost",
       from: "0xe1c6CbB32AD0065Ea52eb66699a5932ECB424a69",
       network_id: 4,
-      timeoutBlocks: 50,
-      confirmations: 0,
-      gas: 4612388
+      gas: 4000000
     }
-
-  // Set default mocha options here, use special reporters etc.
-  /*
-  mocha: {
-     timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
-    }
-    */
   }
 };
